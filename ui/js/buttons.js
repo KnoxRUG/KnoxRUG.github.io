28326341a9f4caa602c75c4bbf43769bc8a1b0aa
wrathematics <wrathematics@gmail.com>
2016-01-29 10:29:01 -0500

    add some text, image to landing page

--- ui/js/buttons.js
+++ ui/js/buttons.js
@@ -1,6 +1,6 @@
-document.body.innerHTML = document.body.innerHTML.replace(/ghbutton/g, '<span class="label label-default" style="background-color:green">GitHub</span>');
-document.body.innerHTML = document.body.innerHTML.replace(/cranbutton/g, '<span class="label label-default" style="background-color:blue">CRAN</span>');
-document.body.innerHTML = document.body.innerHTML.replace(/pdfbutton/g, '<span class="label label-default" style="background-color:red">pdf</span>');
+document.body.innerHTML = document.body.innerHTML.replace(/urlbutton/g, '<span class="label label-default" style="background-color:green">url</span>');
+document.body.innerHTML = document.body.innerHTML.replace(/slidesbutton/g, '<span class="label label-default" style="background-color:red">slides</span>');
+
 
 var links = document.links;
 
