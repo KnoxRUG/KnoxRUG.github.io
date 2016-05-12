f3b33d12ae2d11bb6fabf04ab66a6594c41492bd
Drew Schmidt <wrathematics@gmail.com>
2016-05-12 11:02:29 -0400

    add video link

--- ui/js/buttons.js
+++ ui/js/buttons.js
@@ -1,5 +1,6 @@
 document.body.innerHTML = document.body.innerHTML.replace(/urlbutton/g, '<span class="label label-default" style="background-color:darkgreen">url</span>');
 document.body.innerHTML = document.body.innerHTML.replace(/slidesbutton/g, '<span class="label label-default" style="background-color:darkblue">slides</span>');
+document.body.innerHTML = document.body.innerHTML.replace(/videobutton/g, '<span class="label label-default" style="background-color:red">video</span>');
 
 
 var links = document.links;
